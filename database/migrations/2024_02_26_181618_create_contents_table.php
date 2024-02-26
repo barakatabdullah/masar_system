5a1e987d9da0724d1d99632ab95a35dfabe2ebf1
Abdullah Barakat <abdallah771363885@gmail.com>
2024-02-26 21:17:39 +0300

    Building the database migrations:content

---
+++ database/migrations/2024_02_26_181618_create_contents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('content', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->enum('type', ['text', 'video', 'quiz']);
+            $table->string('title');
+            $table->longText('data'); // Change to BLOB if storing large files
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('content');
+    }
+};
